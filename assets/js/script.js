99854b35a542f469d0f20c25d1adb8b3df4f550e
VadimHadnagy <vhadnagy@edenschool.fr>
2022-10-21 11:43:25 +0300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// let compte = 1000;
+
+// let comptea = 2000;
+
+// for(let i = 1; i < 1000; i++){
+//     compte += 50;
+//     if(compte === comptea){
+//         console.log(i + ' mois pour atteindre 2000')
+//         console.log('1 ans et 8 mois pour atteindre 2000')
+//     }
+// }
+
+
+
+
+
+
